d41c4c5fcaabcdbe543e519c332fce657d2b5919
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-02 22:31:52 +0100

    fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,6 +76,20 @@ HR | Job Board
 		</div>
 	</div>
 
+	<div class="section5 row mt-5">
+		<div class="col-sm-6 _cbg">			
+			<h1 style="font-size: 4em" class="fw-bold mb-5 -1">The #1 Job Board for Graphic Design Jobs</h1>
+			<p class="text-muted mb-5" style="width: 80%">Search and connect with the right candidates faster. This talent seach gives you the opportunity to find candidates who may be a perfect fit for your role</p>
+			<div class="d-flex">
+				<button class="btn px-4 py-2 text-white bg_">Post A Job</button>
+				<button class="btn mx-3" >Learn More</button>
+			</div>
+		</div>
+		<div class="col-sm-6 img_">
+			<img src="https://wp.alithemes.com/html/jobhub/frontend/assets/imgs/blog/img-job.png" alt="">
+		</div>
+	</div>
+
 	<div class="section2 mt-5 mb-4">
 		<h1 class="fw-bold">Browse by category</h1>
 		<div class="d-flex mt-3 justify-content-between">
@@ -109,7 +123,7 @@ HR | Job Board
 		</div>
 
 		<div class="d-flex justify-content-evenly mt-3 flex-wrap">
-			@for ($i = 0; $i < 8; $i++)
+			@for ($i = 0; $i < 6; $i++)
 			<div class="col-sm-4 cont_">
 				<div class="img">
 					<img src="https://wp.alithemes.com/html/jobhub/frontend/assets/imgs/jobs/job-1.png" alt="">
@@ -138,33 +152,6 @@ HR | Job Board
 		</div>
 	</div>
 
-
-	{{-- <div class="section3 mt-5">
-		<h1 class="fw-bold">Recent Jobs</h1>
-		<div class="d-flex mt-3 justify-content-between">
-			<p class="text-muted ft" style="width:50%">8 new opportunities posted today!</p>
-			<div class="d-flex">
-				<p class="mx-3 tg active">Software</p>
-				<p class="mx-3 tg">Design</p>
-				<p class="mx-3 tg">Marketing</p>
-				<p class="mx-3 tg">Service</p>
-				<p class="mx-3 tg">Writing</p>
-				<p class="mx-3 tg">Health Care</p>
-			</div>
-		</div>
-
-		<div class="row mt-3">
-			@for ($i = 0; $i < 8; $i++)
-			<div class="col-sm-4 cont_">
-				<div class="img">
-					<img src="https://wp.alithemes.com/html/jobhub/frontend/assets/imgs/jobs/job-1.png" alt="">
-				</div>
-				<h4 class="mt-3 mb-3">Marketing & Communication</h4>
-				<p class="text-muted ft">156 Available Vacancy</p>
-			</div>				
-			@endfor
-		</div>
-	</div> --}}
 
 </div>
 
