b149ba781fadd35cd8dccbbba8335a3041f5fad0
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-05 13:53:23 +0100

    fixes

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -15,13 +15,28 @@ Login to continue | HR
     </div>
     <div class="form">
         <br>
-        <div class="text-center">
-            <h4 class="text-center mb-4 mt-4 text-white" >Login to continue</h4>
-            <input type="text" placeholder="Enter email address">
-            <input type="password" placeholder="********">
-        </div>
-        <br>
-        <button class="btn px-5 mx-5 btn-outline-light">Login</button>
+        <form action="/login" method="POST">
+            @csrf
+            <div class="text-center">
+                <h4 class="text-center mb-4 mt-4 text-white" >Login to continue</h4>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li style="width: fit-content" class="list-unstyled mb-0">{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+                @if (session("msg"))
+                    <div class="alert alert-danger text-center">
+                        <li style="width: fit-content" class="list-unstyled">{{session("msg")}}</li>
+                    </div>
+                @endif
+                <input name="email" required type="email" placeholder="Enter email address">
+                <input name="password" type="password" placeholder="********">
+            </div>
+            <br>
+            <button class="btn px-5 mx-5 btn-outline-light">Login</button>
+        </form>
         <p class="text-center mt-5 text-white ft">Don't have an account? <a href="/signup" class="text-decoration-none text-primary">Sign up instead</a></p>
     </div>
 </div>
--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -12,16 +12,32 @@ Sign up to continue | HR
 <div class="d-flex login">
     <div class="form" style="height: 100vh; overflow:scroll">
         <br>
-        <div class="text-center">
-            <h4 class="text-center mb-1 text-white" >Sign up to continue</h4>
-            <input type="text" placeholder="First Name">
-            <input type="text" placeholder="Last Name">
-            <input type="text" placeholder="Phone Number">
-            <input type="text" placeholder="Enter email address">
-            <input type="password" placeholder="Password">
-        </div>
-        <br>
-        <button class="btn px-5 mx-5 btn-outline-light">Sign up</button>
+        <form action="/register" method="POST">
+            @csrf
+            <div class="text-center">
+                <h4 class="text-center mb-1 text-white" >Sign up to continue</h4>
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li style="width: fit-content" class="list-unstyled mb-0">{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+                @if (session("msg"))
+                    <div class="alert alert-danger text-center">
+                        <li style="width: fit-content" class="list-unstyled">{{session("msg")}}</li>
+                    </div>
+                @endif
+                
+                <input required type="text" placeholder="First Name">
+                <input required type="text" placeholder="Last Name">
+                <input required type="text" placeholder="Phone Number">
+                <input required type="email" placeholder="Enter email address">
+                <input required type="password" placeholder="Password">
+            </div>
+            <br>
+            <button class="btn px-5 mx-5 btn-outline-light">Sign up</button>
+        </form>
         <p class="text-center mt-5 text-white ft">Already have an account? <a href="/login" class="text-decoration-none text-primary">Login instead</a></p>
     </div>
     <div class="img __reg">
