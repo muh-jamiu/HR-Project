4d25b81ed7aea12ad3f932be3467dd3d8a509fd3
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-05 20:08:23 +0100

    fixes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,39 +22,32 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        $gender = $this->faker->randomElement(['male', 'female']);
-        $relationship = $this->faker->randomElement(['single', 'married', 'divorced', 'complicated']);
-        $education = $this->faker->randomElement(['high school', 'college', 'university']);
-        $body = $this->faker->randomElement(['slim', 'athletic', 'average', 'curvy']);
-        $ethnicity = $this->faker->randomElement(['caucasian', 'african', 'asian', 'hispanic']);
-        $car = $this->faker->randomElement(['mercedez_benz', 'toyota', 'ferrari']);
-        $work_status = $this->faker->randomElement(['employed', 'unemploy','self_unemployed']);
-
         return [
-            'username' => $this->faker->unique()->userName,
+            'username' => $this->faker->userName,
             'email' => $this->faker->unique()->safeEmail,
-            'password' => bcrypt('password'),
+            'password' => bcrypt('password'), 
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
+            'unique_id' => Str::uuid(),
             'avatar' => $this->faker->imageUrl(),
-            'address' => $this->faker->address,
-            'location' => $this->faker->address,
-            'location' => $this->faker->address,
-            'gender' => $gender,
-            'work_status' => $work_status,
-            'car' => $car,
-            'ip_address' => $this->faker->ipv4,
-            'phone_number' => $this->faker->phoneNumber,
-            'birthday' => $this->faker->date(),
-            'height' => $this->faker->numberBetween(110, 280),
-            'hair_color' => $this->faker->safeColorName,
+            'bio' => $this->faker->paragraph,
             'country' => $this->faker->country,
+            'company_name' => $this->faker->company,
+            'company_type' => $this->faker->companySuffix,
+            'company_branch' => $this->faker->word,
             'state' => $this->faker->state,
             'city' => $this->faker->city,
-            'relationship' => $relationship,
-            'education' => $education,
-            'body' => $body,
-            'ethnicity' => $ethnicity,
+            'address' => $this->faker->address,
+            'salary' => $this->faker->numberBetween(30000, 150000),
+            'phone' => $this->faker->phoneNumber,
+            'title' => $this->faker->jobTitle,
+            'education' => $this->faker->word,
+            'gender' => $this->faker->randomElement(['male', 'female']),
+            'is_verified' => $this->faker->boolean,
+            'role' => $this->faker->randomElement(['candidate', 'company']), // Default role
+            'cv' => $this->faker->url,
+            'skills' => implode(', ', $this->faker->words(5)),
+            'experience' => $this->faker->numberBetween(1, 20) . ' years',
         ];
     }
 }
