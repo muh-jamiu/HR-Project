e50c7613e180512fc1112ae6307a8420acb59293
muh-jamiu <ganiujamiu03@gmail.com>
2024-07-05 16:46:41 +0100

    fixes

--- resources/views/components/main-nav.blade.php
+++ resources/views/components/main-nav.blade.php
@@ -5,6 +5,7 @@
     $iscand = $iscand ?? false;
     $iscontact = $iscontact ?? false;
     $isabout = $isabout ?? false;
+    $islogin = session("hr_id");
 @endphp
 
 <div class="navbar d-flex flex-start">
@@ -13,7 +14,11 @@
     <li class="list-unstyled {{$isjobs ? "active" : ""}}"><a href="/browse-jobs" class="text-dark text-decoration-none">Browse Jobs</a></li>
     <li class="list-unstyled {{$isemploy ? "active" : ""}}" ><a href="/employers" class="text-dark text-decoration-none">Employers</a></li>
     <li class="list-unstyled {{$iscand ? "active" : ""}}"><a href="/candidates" class="text-dark text-decoration-none">Candidates</a></li>
-    <li class="list-unstyled"><a href="/get-started" class="text-dark text-decoration-none">Get Started</a></li>
+    @if ($islogin)
+        <li class="list-unstyled"><a href="/account-check" class="text-dark text-decoration-none">Dashboard</a></li>        
+    @else
+        <li class="list-unstyled"><a href="/get-started" class="text-dark text-decoration-none">Get Started</a></li>        
+    @endif
     <li class="list-unstyled {{$iscontact ? "active" : ""}}"><a href="/contact-us" class="text-dark text-decoration-none">Contact Us</a></li>
     <li class="list-unstyled {{$isabout ? "active" : ""}} "><a href="/about-us" class="text-dark text-decoration-none">About Us</a></li>
 </div>
